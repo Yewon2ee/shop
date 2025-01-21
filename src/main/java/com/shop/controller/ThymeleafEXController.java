bc34dd97b3b2260eeb723639a557976480e57f01
Yewon2ee <hyewon8685@naver.com>
2025-01-21 15:52:25 +0900

    Thymeleaf 파라미터 전달

--- src/main/java/com/shop/controller/ThymeleafEXController.java
+++ src/main/java/com/shop/controller/ThymeleafEXController.java
@@ -75,4 +75,11 @@ public class ThymeleafEXController {
     public String thymeleafExample05(){
         return "thymeleafEx/thymeleafEx05";
     }
+
+    @GetMapping(value="/ex06")
+    public String thymeleafExample06(String param1,String param2,Model model){
+        model.addAttribute("param1","param1");
+        model.addAttribute("param2","param2");
+        return "thymeleafEx/thymeleafEx06";
+    }
 }
