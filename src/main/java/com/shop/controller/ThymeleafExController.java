2dc267a8c3d7f944d8de7719e9117ba81cb855b6
Yewon2ee <hyewon8685@naver.com>
2025-01-23 15:18:30 +0900

    bootstrap-header,footer영역 수정

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -5,18 +5,17 @@ import java.util.List;
 import com.shop.dto.ItemDto;
 import java.time.LocalDateTime;
 
-import com.shop.entity.Item;
-import lombok.Getter;
-import org.springframework.cglib.core.Local;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
 @RequestMapping(value="/thymeleaf")
-public class ThymeleafEXController {
+public class ThymeleafExController {
     @GetMapping(value = "/ex01")
     public String thymeleafExample(Model model) {
         model.addAttribute("data", "타임리프 예제 입니다.");
@@ -49,8 +48,8 @@ public class ThymeleafEXController {
             itemDtoList.add(itemDto);
 
         }
-        model.addAttribute("itemDtoLIst",itemDtoList);
-        return"thymeleafEx.thymeleafEx03";
+        model.addAttribute("itemDtoList",itemDtoList);
+        return"thymeleafEx/thymeleafEx03";
     }
 
 
@@ -77,9 +76,15 @@ public class ThymeleafEXController {
     }
 
     @GetMapping(value="/ex06")
-    public String thymeleafExample06(String param1,String param2,Model model){
-        model.addAttribute("param1","param1");
-        model.addAttribute("param2","param2");
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1", param1);  // param1 값을 모델에 추가
+        model.addAttribute("param2", param2);  // param2 값을 모델에 추가
         return "thymeleafEx/thymeleafEx06";
+    }
+
+
+    @GetMapping(value="/ex07")
+    public String thymeleafExample07(){
+        return"thymeleafEx/thymeleafEx07";
     }
 }
